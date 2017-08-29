eeb8dc0d267e4a37219c7e9f3d274c9da029ad55
Akhil <akhilanil.sayone@gmail.com>
2017-08-29 09:59:29 +0530

    last

--- Report/settings.py
+++ Report/settings.py
@@ -111,7 +111,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_2')
 
 
 
